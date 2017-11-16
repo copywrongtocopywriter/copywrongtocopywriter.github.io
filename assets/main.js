d067c7cab4f3473748c0a5c217c4510a7b82d60f
Thomas Tkatchenko <thomas@thank-etc-ok.com>
2017-11-16 17:03:50 +1100

    Add extras

--- assets/main.js
+++ assets/main.js
@@ -6,32 +6,10 @@ $(document).ready(function() {
   var curve = $.bez([0.55, 0, .1, 1]);
   FastClick.attach(document.body);
 
-  // Slideshow
-
-  $('.intro-slideshow').flickity({
-    draggable: false,
-    wrapAround: true,
-    autoPlay: 5500,
-  });
-
-  // Modal behaviour
-
-  $('button').on('click', function() {
-    $('.buy-overlay, .buy-modal').addClass('show');
-  })
-
-  $('.close').on('click', function() {
-    $('.buy-overlay, .buy-modal').removeClass('show');
-  })
-
-  $(window).scroll(function() {
-    onScroll();
-  });
-
-  $(window).resize(function() {
-    onResize();
-  });
-
+  var order = {
+    quantity: 1,
+    shipping: 0
+  };
 
   function onScroll() {
   }
@@ -42,4 +20,214 @@ $(document).ready(function() {
   onScroll();
   onResize();
 
+  $(window).scroll(function() {
+    onScroll();
+  });
+
+  $(window).resize(function() {
+    onResize();
+  });
+
+  // Slideshow
+  $('.intro-slideshow').flickity({
+    draggable: false,
+    wrapAround: true,
+    autoPlay: 5500,
+  });
+
+  // Modal behaviour
+  $('button').on('click', function() {
+    $('.buy-overlay, .buy-modal-wrapper').addClass('show');
+    $('body').addClass('hold');
+
+    ga('send', 'event', {
+        eventCategory: 'Modal',
+        eventAction: 'Opened'
+      }
+    );
+
+  })
+
+  $('.close, .buy-overlay').on('click', function() {
+    $('.buy-overlay, .buy-modal-wrapper').removeClass('show');
+    $('body').removeClass('hold');
+  })
+
+  $('.mailchimp').on('submit', function() {
+    var that = this;
+
+    $.ajax({
+      type: $(this).attr('method'),
+      url: $(this).attr('action').replace('/post?', '/post-json?').concat('&c=?'),
+      data: $(this).serialize(),
+      timeout: 5000,
+      cache: false,
+      dataType: 'jsonp',
+      contentType: 'application/json; charset=utf-8',
+      error: function(err) {
+      },
+      success: function(data) {
+        if (data.result !== 'success') {
+          $('.form-response .error').show();
+        } else {
+          $('.form-response .success').show();
+        }
+
+        setTimeout(function() {
+          $('.form-response div').hide();
+          $(this).find('input[type="submit"]').val('Join');
+        }, 5000);
+      }
+    });
+
+    $(this).find('input[type="submit"]').val('.....');
+
+    return false;
+  });
+
+  $('.country').change(function() {
+    var country = $('.country option:selected').val();
+
+    $('.australia-shipping').hide();
+    $('.shipping-options label').hide();
+    $('.pointer').hide();
+    order.shipping = 0;
+    updateFinalPrice();
+
+    if (country === 'AU') {
+      $('.australia-shipping').show();
+    } else if (country === 'INT') {
+      $('.shipping-options label.international').show();
+      order.shipping = 12;
+      updateFinalPrice();
+      $('.extra .price').show();
+    }
+  });
+
+  $('.get-shipping').on('submit', function() {
+    var that = this;
+
+    var street = $('.street').val();
+    var postcode = $('.postcode').val();
+
+    $.get('https://us-central1-copywrong-to-copywriter.cloudfunctions.net/shipping-api', {street: street, postcode: postcode, countrycode: 'AU', weight: 0.05 + order.quantity * 0.15}).done(function(data) {
+      if (data.success) {
+        var price = 0;
+        data.rates.forEach(function(el) {
+          if (el.service_code === '7D55') {
+            price = el.total_price;
+          }
+        });
+        order.shipping = price;
+        $(that).find('input[type="submit"]').hide();
+        $('.shipping-options label.regular .shipping-cost').html(order.shipping);
+        $('.shipping-options label.regular').show();
+        updateFinalPrice();
+        $('.extra .price').show();
+      } else {
+        $(that).find('input[type="submit"]').val('Sorry, please try again');
+      }
+    });
+
+    $(this).find('input[type="submit"]').val('Calculating...');
+
+    return false;
+  });
+
+  $('#quantity').bind('keyup mouseup', function() {
+    order.quantity = $('#quantity').val();
+    updateFinalPrice();
+  });
+  
+  function updateFinalPrice() {
+    if (order.quantity > 3 ) {
+      $('#quantity').val(3);  
+      order.quantity = 3;
+    }
+
+    if (order.quantity < 1 ) {
+      $('#quantity').val(1);  
+      order.quantity = 1;
+    }
+
+    $('.total-price').html((order.quantity * 25) + order.shipping);
+
+    if (order.shipping > 0) {
+      $('.extra').addClass('show');
+    } else {
+      $('.extra').removeClass('show');
+    }
+
+    $('#paypal-button').html('');
+
+    paypal.Button.render({
+      env: 'sandbox', // Or 'sandbox'
+      client: {
+        sandbox: 'ATi_wJcJ7Cv_pqUWuoKaFsPI-Iq9GLF9C1BwS6f4GTiv4QqILnBC5CBH0N3i68UOg_JMhN3usRyikViS',
+        production: 'AXqKyb-iuzscPaDxD_xJMllWPWOblQ09P9-jjgPq5AjFgpuIlbyO5GE-WqGHwxg7VnTQ9BUlsInVLWDQ'
+      },
+      commit: true, // Show a 'Pay Now' button
+      payment: function(data, actions) {
+        return actions.payment.create({
+          payment: {
+            intent: 'sale',
+            transactions: [
+              {
+                amount: {
+                  total: (order.quantity * 25) + order.shipping,
+                  currency: 'AUD',
+                  details: {
+                    subtotal: order.quantity * 25,
+                    shipping: order.shipping
+                  }
+                },
+                item_list: {
+                  items: [
+                    {
+                      name: 'Copywrong to copywriter',
+                      quantity: order.quantity,
+                      price: 25,
+                      currency: 'AUD'
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        });
+      },
+      onAuthorize: function(data, actions) {
+        return actions.payment.execute().then(function(payment) {
+          ga('send', 'event', {
+              eventCategory: 'Purchase',
+              eventAction: 'Complete'
+            }
+          );
+          ga('require', 'ecommerce');
+          ga('ecommerce:addTransaction', {
+            'id': payment.id,
+            'revenue': (order.quantity * 25) + order.shipping,
+            'shipping': order.shipping,
+            'tax': '0'
+          });
+          ga('ecommerce:addItem', {
+            'id': payment.id,
+            'name': 'Copywrong to copywriter',
+            'price': '25',
+            'quantity': order.quantity
+          });
+          ga('ecommerce:send');
+        });
+      },
+      onCancel: function(data) {
+        ga('send', 'event', {
+            eventCategory: 'Purchase',
+            eventAction: 'Cancelled'
+          }
+        );
+      }
+    }, '#paypal-button');
+  }
+
+  updateFinalPrice();
 });
