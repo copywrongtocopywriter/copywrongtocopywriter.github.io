35bf2916d364742bc26727a2a5d0c075326944f1
Tait Ischia <tait@copygui.de>
2017-11-17 14:16:39 +1100

    Updated Style and 1 other file

--- assets/main.js
+++ assets/main.js
@@ -151,6 +151,11 @@ $(document).ready(function() {
 
     paypal.Button.render({
       env: 'production', // 'production' or 'sandbox'
+      style: {
+        size:  'medium',    // small | medium | large | responsive
+        shape: 'rect',     // pill | rect
+        color: 'black'      // gold | blue | silver | black
+      },
       client: {
         sandbox: 'ATi_wJcJ7Cv_pqUWuoKaFsPI-Iq9GLF9C1BwS6f4GTiv4QqILnBC5CBH0N3i68UOg_JMhN3usRyikViS',
         production: 'AXqKyb-iuzscPaDxD_xJMllWPWOblQ09P9-jjgPq5AjFgpuIlbyO5GE-WqGHwxg7VnTQ9BUlsInVLWDQ'
